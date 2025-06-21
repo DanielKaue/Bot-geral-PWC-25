720e9859478ba698ab33a5913e4b36dbf92d8ea2
Daniel Kauê <danielkaue435@gmail.com>
2025-06-21 20:20:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -960,6 +960,7 @@ async def mods(ctx):
         discord.File("/mnt/data/2fbaa437-8bff-44f3-a799-d694f322d7e7.jar"),
         discord.File("/mnt/data/c67a9d59-3696-4915-ac32-2aff2f27283b.jar"),
         discord.File("/mnt/data/727748f9-bc08-4959-a9f3-06b53e5e1f4b.jar"),
+        discord.File("Em breve"),
         discord.File("/mnt/data/ae981b0e-9cb2-4315-ab4b-ba8d47817dc9.jar")
     ])
 
